e3cc8b48f0e427ef41f55b4e0998a2e8e3d81758
Marco Bassaletti <bassaletti@gmail.com>
2017-10-10 00:41:00 -0300

    Added components to chat page and register in app.module
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ChatPageComponent } from './chat-page/chat-page.component';
 import {appStoreProviders} from './app.store';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatPageComponent
+    ChatPageComponent,
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent,
+    ChatThreadComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule,
