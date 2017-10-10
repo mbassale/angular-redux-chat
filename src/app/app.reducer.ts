fb460fe3acbf635c4dd77605aa87a7c04941ac96
Marco Bassaletti <bassaletti@gmail.com>
2017-10-09 21:50:45 -0300

    Add user and thread state and reducers to root reducer and state.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,16 @@
+
+import {combineReducers, Reducer} from 'redux';
+import {UsersReducer, UsersState} from './user/users.reducer';
+import {ThreadsReducer, ThreadsState} from './thread/threads.reducer';
+
+export interface AppState {
+  users: UsersState;
+  threads: ThreadsState;
+}
+
+const rootReducer: Reducer<AppState> = combineReducers<AppState>({
+  users: UsersReducer,
+  threads: ThreadsReducer
+});
+
+export default rootReducer;
