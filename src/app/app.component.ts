6d7d4095def423f33f5ce963cf467716c3deb17a
Marco Bassaletti <bassaletti@gmail.com>
2017-10-09 22:31:22 -0300

    Added initial data for chat app.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {AppStore} from './app.store';
 import * as Redux from 'redux';
 import {AppState} from './app.reducer';
+import {ChatExampleData} from './data/chat-example-data';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,6 @@ import {AppState} from './app.reducer';
 })
 export class AppComponent {
   constructor(@Inject(AppStore) private store: Redux.Store<AppState>) {
-
+    ChatExampleData(store);
   }
 }
