55e4cde0fdfbe964878aeab6a8c5f1c02c872189
Marco Bassaletti <bassaletti@gmail.com>
2017-10-09 21:50:16 -0300

    Thread state, actions and reducers.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

---
+++ src/app/thread/threads.reducer.ts
@@ -0,0 +1,85 @@
+import {Thread} from './thread.model';
+import {Action} from 'redux';
+import * as ThreadActions from './thread.actions';
+
+export interface ThreadsEntities {
+  [id: string]: Thread;
+}
+
+export interface ThreadsState {
+  ids: string[];
+  entities: ThreadsEntities;
+  currentThreadId?: string;
+}
+
+const initialState: ThreadsState = {
+  ids: [],
+  currentThreadId: null,
+  entities: {}
+};
+
+export const ThreadsReducer =
+  function (state: ThreadsState = initialState, action: Action): ThreadsState {
+    switch (action.type) {
+      case ThreadActions.ADD_THREAD: {
+        const thread = (<ThreadActions.AddThreadAction>action).thread;
+        if (state.ids.includes(thread.id)) {
+          return state;
+        }
+
+        return {
+          ids: [...state.ids, thread.id],
+          currentThreadId: state.currentThreadId,
+          entities: Object.assign({}, state.entities, {
+            [thread.id]: thread
+          })
+        };
+      }
+      case ThreadActions.ADD_MESSAGE: {
+        const thread = (<ThreadActions.AddMessageAction>action).thread;
+        const message = (<ThreadActions.AddMessageAction>action).message;
+
+        // if message is sent to current thread, mark it as read
+        const isRead = message.thread.id === state.currentThreadId ? true : message.isRead;
+        const newMessage = Object.assign({}, message, {isRead: isRead});
+
+        // get the old thread
+        const oldThread = state.entities[thread.id];
+
+        // create a new thread with this new message
+        const newThread = Object.assign({}, oldThread, {
+          messages: [...oldThread.messages, newMessage]
+        });
+
+        return {
+          ids: state.ids,
+          currentThreadId: state.currentThreadId,
+          entities: Object.assign({}, state.entities, {
+            [thread.id]: newThread
+          })
+        };
+      }
+      case ThreadActions.SELECT_THREAD: {
+        const thread = (<ThreadActions.SelectThreadAction>action).thread;
+        const oldThread = state.entities[thread.id];
+
+        // mark messages as read
+        const newMessages = oldThread.messages.map(
+          (message) => Object.assign({}, message, {isRead:true})
+        );
+
+        // make a new thread
+        const newThread = Object.assign({}, oldThread, {messages: newMessages});
+
+        return {
+          ids: state.ids,
+          currentThreadId: thread.id,
+          entities: Object.assign({}, state.entities, {
+            [thread.id]: newThread
+          })
+        };
+      }
+      default:
+        return state;
+    }
+  };
